25ca494f4f7803ecbeef291568671fcdc511181a
Angelica <Angelica137@users.noreply.github.com>
2022-02-13 22:08:03 +0000

    test .value_of_card() returns 4, red

--- tests/test_black_jack.py
+++ tests/test_black_jack.py
@@ -3,3 +3,7 @@ from scripts.black_jack import *
 
 def test_value_of_card_returns_10():
     assert value_of_card('K') == 10
+
+
+def test_value_of_card_returns_4():
+    assert value_of_card('4') == 4
