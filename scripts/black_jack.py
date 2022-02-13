088b5a7e0b6f90c8a30ab73da3cf9d32fd50acf4
Angelica <Angelica137@users.noreply.github.com>
2022-02-13 22:12:49 +0000

    test .value_of_card() returns 1, red

--- scripts/black_jack.py
+++ scripts/black_jack.py
@@ -1,4 +1,9 @@
 def value_of_card(card: str) -> int:
+    """
+    Returns the numeric value of a card.
+    Cards Jack, Queen and K return 10
+    Ace returns 1
+    """
     if card == 'K':
         return 10
     return int(card)
--- tests/test_black_jack.py
+++ tests/test_black_jack.py
@@ -7,3 +7,7 @@ def test_value_of_card_returns_10():
 
 def test_value_of_card_returns_4():
     assert value_of_card('4') == 4
+
+
+def test_value_of_card_returns_1():
+    assert value_of_card('A') == 1
