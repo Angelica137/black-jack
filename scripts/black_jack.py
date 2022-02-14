958ca878f975566c859e0963567d187be7d14a2d
Angelica <Angelica137@users.noreply.github.com>
2022-02-14 21:23:57 +0000

    refactor .highre_card()

--- scripts/black_jack.py
+++ scripts/black_jack.py
@@ -16,15 +16,8 @@ def higher_card(card_one: str, card_two: str) -> str:
     Returns the highest value card, if both are the same
     value, both are returned
     """
-    letters = ['J', 'Q', 'K', 'A']
-    if card_one in letters:
-        card_one_value = value_of_card(card_one)
-    else:
-        card_one_value = int(card_one)
-    if card_two in letters:
-        card_two_value = value_of_card(card_two)
-    else:
-        card_two_value = int(card_two)
+    card_one_value = value_of_card(card_one)
+    card_two_value = value_of_card(card_two)
     if card_one_value > card_two_value:
         return card_one
     if card_one_value < card_two_value:
