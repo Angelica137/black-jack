39c618bc3467ec80a79b17b708e59b763b4380e3
Angelica <Angelica137@users.noreply.github.com>
2022-02-17 12:31:48 +0000

    refactor value_of_card

--- scripts/black_jack.py
+++ scripts/black_jack.py
@@ -4,7 +4,7 @@ def value_of_card(card: str) -> int:
     Cards Jack, Queen and K return 10
     Ace returns 1
     """
-    if card == 'J' or card == 'Q' or card == 'K':
+    if card in 'JQK':
         return 10
     if card == 'A':
         return 1
